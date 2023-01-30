f57fe2c653f94e75a1caaefa3e060209e187c2ad
vanis <vanis@PC963>
2023-01-30 08:54:13 +0100

    Add project files.

---
+++ databaseConnect/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SqlClient;
+
+namespace databaseConnect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder consStringBuilder = new SqlConnectionStringBuilder();
+            consStringBuilder.UserID = "sa";
+            consStringBuilder.Password = "student";
+            consStringBuilder.InitialCatalog = "firstConnect";
+            consStringBuilder.DataSource = "PC963";
+            consStringBuilder.ConnectTimeout = 3;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(consStringBuilder.ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Pripojeno");
+
+
+
+                    client client = new client("Novak", "Dw41vr2x") ;
+                    //client.insert(connection);
+
+                    //client.select(connection, 2);
+
+                    client.selectAll(connection);
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ databaseConnect/client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databaseConnect
+{
+    internal class client
+    {
+
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int ID { get; set; }
+
+        public client(string name, string password)
+        {
+            Name = name;
+            Password = password;
+
+        }
+
+        public void insert(SqlConnection connection)
+        {
+            string insert = $"insert into {nameof(client)} ({nameof(Name)}, {nameof(Password)}) values ('{Name}', '{Password}')";
+            using (SqlCommand command = new SqlCommand(insert, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void select(SqlConnection connection, int id)
+        {
+
+            string select = $"select * from {nameof(client)} where id = {id}";
+
+            using (SqlCommand command = new SqlCommand(select, connection))
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{nameof(Name)}: {reader[0].ToString()}\n" +
+                                      $"{nameof(Password)}: {reader[1].ToString()}\n" +
+                                      $"{nameof(Password)}: {reader[1].ToString()}");
+                }
+            }
+        }
+
+        public void selectAll(SqlConnection connection)
+        {
+
+            string selectAll = $"select * from {nameof(client)}";
+            using (SqlCommand command = new SqlCommand(selectAll, connection))
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{nameof(Name)}: {reader[0].ToString()}\n" +
+                                      $"{nameof(Password)}: {reader[1].ToString()}\n" +
+                                      $"-------------------------------------------");
+                }
+            }
+        }
+
+
+
+    }
+}
